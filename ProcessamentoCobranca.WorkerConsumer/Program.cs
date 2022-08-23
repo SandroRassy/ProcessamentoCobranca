0fc6e6f7967299e90e7cbc4890ca86860c34166f
Sandro Rassy <sandrorassy@gmail.com>
2022-08-23 19:54:03 -0300

    acerto no robo

--- ProcessamentoCobranca.WorkerConsumer/Program.cs
+++ ProcessamentoCobranca.WorkerConsumer/Program.cs
@@ -7,57 +7,48 @@ using ProcessamentoCobranca.Services.Extensions;
 using ProcessamentoCobranca.WorkerConsumer.Workers;
 using Serilog;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.AddSerilog("Worker MassTransit");
+Log.Information("Starting Worker");
+
+var configuration = new ConfigurationBuilder()
+.AddJsonFile("appsettings.json")
+.Build();
+
+var host = Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration(builder =>
+{
+    builder.Sources.Clear();
+    builder.AddConfiguration(configuration);
+})
+.ConfigureServices((context, collection) =>
+{
+    collection.AddMassTransitExtension(context.Configuration);
+})
+.Build();
+
+
+var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+{
+    var rabbitMQSettings = builder.Configuration.GetConnectionString("RabbitMq");
+    cfg.Host(rabbitMQSettings);
+    cfg.ReceiveEndpoint("CalculoConsumo", e =>
+    {
+        var teste = e.InputAddress;
+        e.PrefetchCount = 10;
+        e.UseMessageRetry(p => p.Interval(3, 100));
+        e.Consumer<WorkerCalculoConsumo>();
+    });
+});
+
+await busControl.StartAsync(new CancellationToken());
+
 try
 {
-    var builder = WebApplication.CreateBuilder(args);
-    builder.AddSerilog("Worker MassTransit");
-    Log.Information("Starting Worker");
-
-    var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .Build();
-
-    var host = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration(builder =>
-    {
-        builder.Sources.Clear();
-        builder.AddConfiguration(configuration);
-    })
-    .ConfigureServices((context, collection) =>
-    {
-        collection.AddMassTransitExtension(context.Configuration);
-    })
-    .Build();
-
-
-    var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
-    {
-        var rabbitMQSettings = builder.Configuration.GetConnectionString("RabbitMq");
-        cfg.Host(rabbitMQSettings);
-        cfg.ReceiveEndpoint("CalculoConsumo", e =>
-        {
-            var teste = e.InputAddress;
-            e.PrefetchCount = 10;
-            e.UseMessageRetry(p => p.Interval(3, 100));
-            e.Consumer<WorkerCalculoConsumo>();
-        });
-    });
-
-    await busControl.StartAsync(new CancellationToken());
-
-    try
-    {
-        Console.WriteLine("Press enter to exit");
-        await Task.Run(() => Console.ReadLine());
-    }
-    finally
-    {
-        await busControl.StopAsync();
-    }
-    
+    Console.WriteLine("Press enter to exit");
+    await Task.Run(() => Console.ReadLine());
 }
-catch (Exception)
+finally
 {
-
-    throw;
+    await busControl.StopAsync();
 }
