0ac1613568ed3d3b4380bb11c78ec9986b0a2628
Sandro Rassy <sandrorassy@gmail.com>
2022-08-23 18:34:08 -0300

    TesteRelatorio

--- ProcessamentoCobranca.API/Controllers/RelatorioConsumoController.cs
+++ ProcessamentoCobranca.API/Controllers/RelatorioConsumoController.cs
@@ -10,14 +10,12 @@ namespace ProcessamentoCobranca.API.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class RelatorioConsumoController : ControllerBase
-    {
-        private readonly IPublishEndpoint _publishEndpoint;
+    {        
         private readonly ICobrancaConsumoServices _cobrancaConsumoServices;
 
 
-        public RelatorioConsumoController(IPublishEndpoint publishEndpoint, ICobrancaConsumoServices cobrancaConsumoServices)
+        public RelatorioConsumoController(ICobrancaConsumoServices cobrancaConsumoServices)
         {
-            _publishEndpoint = publishEndpoint;
             _cobrancaConsumoServices = cobrancaConsumoServices;
         }
 
